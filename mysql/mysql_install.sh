71cb8c9ab90ab60e2d342d20bcffb0808db9bddd
shanzhiliu <1350700145@qq.com>
2020-07-26 21:16:43 +0800

    add mysql

---
+++ mysql/mysql_install.sh
@@ -0,0 +1,9 @@
+wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+yum localinstall mysql57-community-release-el7-11.noarch.rpm
+yum repolist enabled | grep "mysql.*-community.*"
+yum install -y mysql-community-server
+systemctl start mysqld
+systemctl status mysqld
+
+#查看密码
+grep 'temporary password' /var/log/mysqld.log
