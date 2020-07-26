cda2de5065f863642c0c1c03041b6cadab46bc0e
shanzhiliu <1350700145@qq.com>
2020-07-26 20:10:37 +0800

    add hive

---
+++ hive/hive_install.sh
@@ -0,0 +1,64 @@
+
+wget https://downloads.apache.org/hive/hive-1.2.2/apache-hive-1.2.2-bin.tar.gz
+
+cudir=`pwd`
+cuuser=shanshan3
+
+#参数1 java 要安装的路径
+hive_install_path=/opt/soft/hive-1.2.2
+#参数2 java 压缩包的位置
+hive_jar_path="$cudir/apache-hive-1.2.2-bin.tar.gz"
+
+hive_install_dir_name=/opt/soft/test
+hive_install_file_name=jdk1.8
+
+hive_jar_dir_name=/opt/soft/test
+hive_jar_file_name=jdk1.8.tar
+
+
+if [ -n "$1" ]; then
+    hive_install_path=$1
+    #获取文件名
+    hive_install_dir_name=${hive_install_path%/*}
+    echo $hive_install_dir_name
+    #获取所在路径
+    hive_install_file_name=${hive_install_path##*/}
+    echo $hive_install_file_name
+else
+    echo "使用配置文件的默认路径安装"
+      #获取文件名
+    hive_install_dir_name=${hive_install_path%/*}
+    echo $hive_install_dir_name
+    #获取所在路径
+    hive_install_file_name=${hive_install_path##*/}
+    echo $hive_install_file_name
+fi
+
+
+hive_jar_dir_name=${hive_jar_path%/*}
+echo $hive_jar_dir_name
+#获取所在路径
+hive_jar_file_name=${hive_jar_path##*/}
+echo $hive_jar_file_name
+    
+
+
+if [ ! -d "$hive_install_dir_name" ];
+then
+ mkdir -p  $hive_install_dir_name
+fi
+
+mv $hive_jar_path $hive_install_dir_name
+cd $hive_install_dir_name
+
+
+mkdir ./hive && tar -xzvf  $hive_jar_file_name -C ./hive --strip-components 1
+mv hive $hive_install_file_name
+
+
+chown -R $cuuser:$cuuser apache-hive-1.2.2-bin/
+chmod -R 755  apache-hive-1.2.2-bin/	
+
+
+echo "hive 安装配置完成"
+
---
+++ hive/hive_install_other.sh
@@ -0,0 +1,37 @@
+
+CONFIG_PATH="host.ini"
+CONFIGS=()
+
+# 读取配置文件
+if [ -f ${CONFIG_PATH} ]; then
+    CONFIGS=()
+    while read line
+    do
+        # 注释行或空行 跳过
+        if [[ ${line:0:1} == "#" || $line =~ ^$ ]]; then
+            continue
+        else
+            CONFIGS+=("$line")
+        fi
+    done < ${CONFIG_PATH}
+fi
+
+#服务器配置数
+CONFIG_LENGTH=${#CONFIGS[*]}  #配置站点个数
+
+
+function ConfigRunShell(){
+    echo -e "连接到其他服务器运行脚本--开始\t"
+    for ((i=1;i<${CONFIG_LENGTH};i++));
+    do
+        CONFIG=(${CONFIGS[$i]}) #将一维sites字符串赋值到数组
+        scp hive_install.sh  ${CONFIG[4]}@${CONFIG[3]}:~
+        ssh -T -p ${CONFIG[2]} ${CONFIG[4]}@${CONFIG[3]} << EOF 
+        sh hive_install.sh 
+EOF
+    done
+    
+     echo -e "连接到其他服务器运行脚本---完成\t"
+}
+
+ConfigRunShell
