ea3c0f1fdb23c78369b0530f6ec022a26f288d87
shanzhiliu <1350700145@qq.com>
2020-07-27 09:27:47 +0800

    zk shell

--- zookeeper/zookeeper_install.sh
+++ zookeeper/zookeeper_install.sh
@@ -4,21 +4,38 @@ cuuser=shanshan3
 
 basepath=/opt/soft/
 zkname=zookeeper-3.4.2
-zkpath=$basepath$zkname"
+zkpath="$basepath$zkname"
+zkmyidpath="$zkpath/mydata/"
+
+myidcontext=1
+
+
+if [ -n "$1" ]; then
+     myidcontext=$1
+else
+    
+fi
+
+
+/opt/soft/zookeeper-3.4.2/mydata
 
 tar -xzvf zookeeper-3.4.2.tar.gz -C $basepath
+
+#创建myid存放路径
+mkdir -p $zkmyidpath
+
+echo $myidcontext >> myid
+#拷贝配置文件
+cp zoo.cfg "$zkpath/conf"
 
 chown -R $cuuser:$cuuser $zkpath
 
 chmod -R 755 $zkpath
 
-#zookeer config
-export ZOOKEEPER_HOME=$zkpath
 
-
-echo "zookeer config" >> ~/.bash_profile
+echo "#zookeer config" >> ~/.bash_profile
 echo "export ZOOKEEPER_HOME=$zkpath" >> ~/.bash_profile
-echo "export PATH=$PATH:$ZOOKEEPER_HOME/bin:$PATH:$ZOOKEEPER_HOME/sbin"  >> ~/.bash_profile
+echo "export PATH=\$PATH:\$ZOOKEEPER_HOME/bin:\$PATH:\$ZOOKEEPER_HOME/sbin"  >> ~/.bash_profile
 
 
 source ~/.bash_profile
