2de5dd5f9983139bc0cc037f169c56a3dadadac8
shanzhiliu <1350700145@qq.com>
2020-07-28 08:32:36 +0800

    拆分函数

--- common/install_all.sh
+++ common/install_all.sh
@@ -1,4 +1,6 @@
+
 #--------------------本机root---------------------
+function installBaseLocal(){
 #配置本机host.ini
 #本机安装基础软件
 sh base_install.sh
@@ -6,9 +8,11 @@ sh base_install.sh
 sh host_install.sh
 #本机配置免密登录
 sh nopasswd_install.sh
+}
+
 
 #--------------------其他机器root---------------------
-
+function installBaseOther(){
 #拷贝host.ini到其他机器
 sh config_install_other.sh
 #其他机器安装基础软件
@@ -17,22 +21,31 @@ sh base_install_other.sh
 sh host_install_other.sh
 #其他机器配置免密登录
 sh nopasswd_install_other.sh
+}
 
-
+#安装java
+function installJava(){
 #本机安装java
 sh java_install.sh
 #其他机器安装java
 sh java_install_other.sh
 
 source ~/.bash_profile
+}
 
+#添加shanshan3用户
+function addLinuxUser(){
 #本机shanshan3用户
 sh user_config.sh
 #其他机器shanshan3用户
 sh user_config_other.sh
 #执行其他用的免密登录
 sh user_nopasswd.sh
+}
 
+
+#安装配置hadoop
+function installHadoop(){
 #hadoop_slaves文件修改
 sh hadoop_config_slaves.sh
 #本机安装
@@ -47,46 +60,50 @@ sh hadoop_config.sh
 #其他机器配置hadoop
 sh hadoop_config_other.sh
 
+#.base_profile from root to user
+sh profile_install.sh
+sh profile_install_other.sh
+
 #本机hadoop 修改所属用户
 sh hadoop_chown.sh
 #其他机器hadoop 修改所属用户
 sh hadoop_chown_other.sh
+#格式化hadoop
+sh hadoop_format.sh
+}
 
+
+#安装zookeeper
+function installZookeeper(){
+#本机安装zookeeper
+sh zookeeper_install.sh
+#其他机器安装zookeeper
+sh zookeeper_install_other.sh
 
 #.base_profile from root to user
 sh profile_install.sh
 sh profile_install_other.sh
 
-#格式化hadoop
-sh hadoop_format.sh
-
-
-#本机安装zookeeper
-sh zookeeper_install.sh
-#其他机器安装zookeeper
-sh zookeeper_install_other.sh
 #使生效
 source ~/.bash_profile
+}
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#--------------------本机root（基础软件，免密登录）---------------------
+installBaseLocal
+#--------------------其他机器root（基础软件，免密登录）---------------------
+installBaseOther
+#安装java
+installJava
+#添加shanshan3用户
+addLinuxUser
+#安装配置hadoop
+installHadoop
+#安装zookeeper
+installZookeeper
 
 
 
