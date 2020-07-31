06be3c0010be07d9c2e9c7eccda2e0e22100eeca
shanzhiliu <1350700145@qq.com>
2020-07-31 15:01:53 +0800

    check cn

--- common/download.sh
+++ common/download.sh
@@ -1,73 +1,73 @@
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/install_all.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/install_all.sh
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/base_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/base_install_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/base_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/base_install_other.sh
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/config_install_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/config_install_other.sh
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/profile_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/profile_install_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/profile_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/profile_install_other.sh
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/host_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/host_install_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/host_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/host_install_other.sh
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/user_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/user_install_other.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/user_nopasswd.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/user_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/user_install_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/user_nopasswd.sh
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/host.ini
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/user_host.ini
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/host.ini
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/user_host.ini
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/user_config.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/common/user_config_other.sh
-
-
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/ssh/nopasswd_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/ssh/nopasswd_install_other.sh
-
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/java/java_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/java/java_install_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/user_config.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/common/user_config_other.sh
 
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoop_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoop_install_other.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoop_config.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoop_config_other.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoop_chown.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoop_chown_other.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoop_format.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoop_config_slaves.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/ssh/nopasswd_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/ssh/nopasswd_install_other.sh
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoopdata/hadoop-env.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoopdata/yarn-env.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoopdata/core-site.xml
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoopdata/hdfs-site.xml
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoopdata/mapred-site.xml
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoopdata/yarn-site.xml
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hadoop/hadoopdata/slaves
-
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hive/hive_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hive/conf/hive-site.xml
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/hive/hive_mysql_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/java/java_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/java/java_install_other.sh
 
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/mysql/mysql_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoop_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoop_install_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoop_config.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoop_config_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoop_chown.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoop_chown_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoop_format.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoop_config_slaves.sh
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/zookeeper/zookeeper_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/zookeeper/zookeeper_install_other.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/zookeeper/zookeeper_config.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/zookeeper/zookeeper_start_all.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/zookeeper/zookeeper_stop_all.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/zookeeper/conf/zoo.cfg
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoopdata/hadoop-env.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoopdata/yarn-env.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoopdata/core-site.xml
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoopdata/hdfs-site.xml
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoopdata/mapred-site.xml
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoopdata/yarn-site.xml
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hadoop/hadoopdata/slaves
+
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hive/hive_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hive/conf/hive-site.xml
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/hive/hive_mysql_install.sh
+
+
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/mysql/mysql_install.sh
+
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/zookeeper/zookeeper_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/zookeeper/zookeeper_install_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/zookeeper/zookeeper_config.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/zookeeper/zookeeper_start_all.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/zookeeper/zookeeper_stop_all.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/zookeeper/conf/zoo.cfg
 
 
 #--no-check-certificate --no-cache --no-cookies
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/kafka/kafka_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/kafka/kafka_install_all.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/kafka/kafka_start_all.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/kafka/kafka_stop_all.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/kafka/kafka_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/kafka/kafka_install_all.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/kafka/kafka_start_all.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/kafka/kafka_stop_all.sh
 
 
 
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/scala/scala_install.sh
-wget https://raw.githubusercontent.com/shanzhiliu/script/master/scala/scala_install_other.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/scala/scala_install.sh
+wget https://raw.githubusercontent.com/shanzhiliu/script/cn/scala/scala_install_other.sh
