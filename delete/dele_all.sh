9ae42098e751c0386dd7d11941ad01801b05834b
shanzhiliu <1350700145@qq.com>
2020-07-31 23:05:51 +0800

    delete sh

---
+++ delete/dele_all.sh
@@ -0,0 +1,12 @@
+rm -rf /root/*
+rm -rf /root/.ssh
+rm -rf /opt/soft/
+
+echo "" >  /etc/hosts
+
+sed -i '13,2000d' /root/.bash_profile
+userdel -r shanshan3
+
+#rm -rf /home/shanshan3/*
+#rm -rf /home/shanshan3/.ssh
+
