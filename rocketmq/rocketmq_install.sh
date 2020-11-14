2bd605403ef6006d18c57e81ead722ab476c75b9
shanzhiliu <1350700145@qq.com>
2020-11-14 21:10:03 +0800

    rocketmq 脚本

--- rocketmq/rocketmq_install.sh
+++ rocketmq/rocketmq_install.sh
@@ -4,7 +4,7 @@
 wget  https://downloads.apache.org/rocketmq/4.7.1/rocketmq-all-4.7.1-bin-release.zip
 
 #java 安装路径
-rocketmq_path=/opt/soft/rocketmq-4.7.1
+rocketmq_path=/opt/soft/
 #下载下来的压缩包名称
 rocketmq_zip_name=rocketmq-all-4.7.1-bin-release.zip 
 # 如果不存在文件夹 就创建
@@ -14,10 +14,15 @@ then
 fi
 
 #解压到指定的java路径中
-tar -xzvf  $rocketmq_zip_name -C $rocketmq_path --strip-components 1
-
+unzip  $rocketmq_zip_name -d $rocketmq_path
+#删除压缩包
 rm -rf $rocketmq_zip_name
+#进入目录
+cd $rocketmq_path
+#修改名称
+mv $rocketmq_zip_name rocketmq-4.7.1
 
+rocketmq_path=/opt/soft/rocketmq-4.7.1
 # 环境变量修改
 echo "#rocketmq config" >> ~/.bash_profile
 echo "export ROCKETMQ_HOME=$rocketmq_path" >> ~/.bash_profile
