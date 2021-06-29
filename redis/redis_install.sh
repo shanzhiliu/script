2b868b4b89d5187ae36dbaad4e73162aaac264cc
shanzhiliu <1350700145@qq.com>
2021-06-29 20:01:00 +0800

    add redis shell

--- redis/redis_install.sh
+++ redis/redis_install.sh
@@ -1,3 +1,5 @@
+# wget https://raw.githubusercontent.com/shanzhiliu/script/newShell/redis/redis_install.sh
+
 yum install -y gcc-c++
 yum install -y wget
 wget http://download.redis.io/releases/redis-5.0.4.tar.gz
@@ -5,4 +7,16 @@ tar -zxvf redis-5.0.4.tar.gz
  cd redis-5.0.4
  make
  make install PREFIX=/opt/soft/redis
- 
+
+java_path=/opt/soft/redis
+
+echo "#redis config" >> ~/.bash_profile
+echo "export REDIS_HOME=$java_path" >> ~/.bash_profile
+echo "export PATH=\$PATH:\$REDIS_HOME/bin:\$PATH:\$REDIS_HOME/sbin"  >> ~/.bash_profile
+
+
+
+
+
+# cd /opt/soft/redis/bin
+# ./redis-server
